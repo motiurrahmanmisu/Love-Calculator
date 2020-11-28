3796ca8c4cecbc965e74ce747dc92f5ac070d4df
Motiur Rahman <74164986+motiurrahmanmisu@users.noreply.github.com>
2020-11-28 14:37:20 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var random=Math.floor(Math.random() * 100 + 1);
+document.querySelector("#click").addEventListener("click",function(){
+	var text1=document.querySelector("#text");
+var result=document.querySelector("#result");
+var random=Math.floor(Math.random() * 100 + 1);
+	if(text1.value==""){
+		alert("Please type any name");
+	}else{
+		result.innerHTML="<b style='color:green;'>You Love your favourite person</b>"+" = "+random+"%";
+	}
+});
